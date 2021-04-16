93ca1de19a32e2b43d590460f856b26a41fbcf1a
sayat-bey <sayat.bey@ya.ru>
2021-04-16 20:58:16 +0600

    1.3

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ try:
             current_time = now.strftime("%H:%M")
             result = subprocess.run(["ping", "-c", "2", ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)             
             if result.returncode == 0:
-                print(f'\r[PING OK],    timestamp: {current_time}, {hostname:22} cycle: {1}', end='')
+                print(f'\r[PING OK],    timestamp: {current_time}, {hostname:22} cycle: {i}', end='')
             else:
-                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname:22} cycle: {1}')
+                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname:22} cycle: {i}')
         i += 1
 
 except KeyboardInterrupt:
