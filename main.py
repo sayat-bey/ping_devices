dbf3131fa2abf66adbdff7dea96a8953278fb13d
sayat-bey <sayat.bey@ya.ru>
2021-04-16 20:55:58 +0600

    1.2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import subprocess
-import time
 import yaml
 from datetime import datetime
 
@@ -10,6 +9,7 @@ with open("devices.yaml", "r") as file:
 print("-------------------------------------------------------------------------------------------------------")
 
 try:
+    i = 1
     while True:
         output = []
         for hostname, ip in devices.items():
@@ -17,11 +17,10 @@ try:
             current_time = now.strftime("%H:%M")
             result = subprocess.run(["ping", "-c", "2", ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)             
             if result.returncode == 0:
-                print(f'\r[PING OK],    timestamp: {current_time}, {hostname}               ', end='')
+                print(f'\r[PING OK],    timestamp: {current_time}, {hostname:22} cycle: {1}', end='')
             else:
-                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname}               ')
-        
-    time.sleep(60)
+                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname:22} cycle: {1}')
+        i += 1
 
 except KeyboardInterrupt:
     print("\n")
