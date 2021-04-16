8300a90126b86680bd19f2090370b1d4d9a98e2d
sayat-bey <sayat.bey@ya.ru>
2021-04-16 20:50:21 +0600

    1.2

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ try:
             current_time = now.strftime("%H:%M")
             result = subprocess.run(["ping", "-c", "2", ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)             
             if result.returncode == 0:
-                print(f'\r[PING OK],    timestamp: {current_time}, {hostname}', end='')
+                print(f'\r[PING OK],    timestamp: {current_time}, {hostname}               ', end='')
             else:
-                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname}')
+                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname}               ')
         
     time.sleep(60)
 
