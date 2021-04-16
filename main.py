f33aa49242c98db58cd73ecfd5e816bcaa0a4eaf
sayat-bey <sayat.bey@ya.ru>
2021-04-16 16:16:49 +0600

    v1.1

---
+++ main.py
@@ -0,0 +1,29 @@
+import subprocess
+import time
+import yaml
+from datetime import datetime
+
+
+with open("devices.yaml", "r") as file:
+    devices = yaml.load(file, yaml.SafeLoader)  # hostname : ip
+
+print("-------------------------------------------------------------------------------------------------------")
+
+try:
+    while True:
+        output = []
+        for hostname, ip in devices.items():
+            now = datetime.now()
+            current_time = now.strftime("%H:%M")
+            result = subprocess.run(["ping", "-c", "2", ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)             
+            if result.returncode == 0:
+                print(f'\r[PING OK],    timestamp: {current_time}, {hostname}', end='')
+            else:
+                print(f'\n[PING ERROR], timestamp: {current_time}, {hostname}')
+        
+    time.sleep(60)
+
+except KeyboardInterrupt:
+    print("\n")
+    print("-------------------------------------------------------------------------------------------------------")
+    print("script cancelled")
